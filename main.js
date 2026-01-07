f69e44303cd6fd075aa24c048e71e6aa4b22b3e0
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-01-07 15:33:37 +0530

    Wait for DOM to load

---
+++ main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    window.addEventListener('load', () => {
+    setTimeout(() => {
+      document.querySelector('.loader').classList.add('hidden');
+    }, 1500);
+  });
