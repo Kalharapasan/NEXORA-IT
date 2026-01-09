97e69ff0d73a8a723cacc3fa33460d94d943e989
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-01-09 14:44:49 +0530

    Validate and sanitize input

--- php/contact.php
+++ php/contact.php
@@ -18,4 +18,11 @@ $response = [
     'message' => ''
 ];
 
+function sanitize_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
 ?>
